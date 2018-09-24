8b3dc3d2ada91cab98a849d09f95d2396860ebc5
unknown <joehuldens@gmail.com>
2018-09-24 23:31:02 +0200

    helloj

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,6 +17,6 @@ public class Greeter {
   * @return greeting string
   */
   public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Helloj, %s!", someone);
   }
 }
